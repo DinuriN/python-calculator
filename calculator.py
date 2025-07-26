c2cdc7e857fb35fe973e586285e46bcaa97adc3b
DinuriN <dinurin730@gmail.com>
2025-07-26 15:24:57 +0530

    Update main block for clearer CLI usage

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import sys
+
 def add (a,b):
     return a+b
 
@@ -13,23 +15,45 @@ def divide(a,b):
     return a/b
 
 if __name__=="__main__":
-    print("Simple Calculator")
-    print("Operations: Add, Subtract, Multiply, Divide")
+    if len(sys.argv) != 4:
+        print("Usage: python calculator.py <operation> <a> <b>")
+        sys.exit(1)
 
-    a=float(input("Enter first number: "))
-    operation = input("Enter operation: +, -, *, /: ").strip().lower()
-    b=float(input("Enter second number: "))
+    operation, a, b = sys.argv[1], float(sys.argv[2]), float(sys.argv[3])
 
-    if operation == "+":
-        print("Answer: ", add(a,b))
-    elif operation == "-":
-        print("Answer: ", subtract(a,b))
-    elif operation== "*":
-        print("Answer: ", multiply(a,b))
-    elif operation == "/":
+    operations = {
+        "add": add,
+        "subtract": subtract,
+        "multiply": multiply,
+        "divide": divide,
+    }
+
+    if operation in operations:
         try:
-            print("Answer: ", divide(a,b))
+            print("Answer: ", operations[operation](a,b))
         except ValueError as e:
             print(e)
     else:
-        print("Invalid operation. Try again.")
+        print("Invalid Operation. Try again")
+
+
+    # print("Simple Calculator")
+    # print("Operations: Add, Subtract, Multiply, Divide")
+
+    # a=float(input("Enter first number: "))
+    # operation = input("Enter operation: +, -, *, /: ").strip().lower()
+    # b=float(input("Enter second number: "))
+
+    # if operation == "+":
+    #     print("Answer: ", add(a,b))
+    # elif operation == "-":
+    #     print("Answer: ", subtract(a,b))
+    # elif operation== "*":
+    #     print("Answer: ", multiply(a,b))
+    # elif operation == "/":
+    #     try:
+    #         print("Answer: ", divide(a,b))
+    #     except ValueError as e:
+    #         print(e)
+    # else:
+    #     print("Invalid operation. Try again.")
